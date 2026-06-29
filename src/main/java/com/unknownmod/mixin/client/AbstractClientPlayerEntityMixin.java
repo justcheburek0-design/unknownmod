8ccb0fadabe9a5a23b6ed66fd8b03bdbea8f8ad7
CodeSnap Developer <codesnap@nightdev.io>
2026-06-29 07:40:47 +0000

    fix: use Mojang-mapped field name playerInfo for MC 26.1.2
    MC 26.1.2 renamed playerListEntry → playerInfo in AbstractClientPlayer.
    Also GameProfile is now a Record in authlib 7.x (no getProperties()).

--- src/main/java/com/unknownmod/mixin/client/AbstractClientPlayerEntityMixin.java
+++ src/main/java/com/unknownmod/mixin/client/AbstractClientPlayerEntityMixin.java
@@ -1,7 +1,6 @@
 package com.unknownmod.mixin.client;
 
 import com.mojang.authlib.GameProfile;
-import com.unknownmod.util.ProfileApplier;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.Minecraft;
@@ -9,6 +8,7 @@ import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.client.multiplayer.PlayerInfo;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -16,6 +16,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Environment(EnvType.CLIENT)
 @Mixin(net.minecraft.client.player.AbstractClientPlayer.class)
 public abstract class AbstractClientPlayerEntityMixin {
+    @Shadow
+    private PlayerInfo playerInfo;
 
     @Inject(method = "getPlayerInfo", at = @At("RETURN"), cancellable = true)
     private void unknownmod$refreshCachedEntry(CallbackInfoReturnable<PlayerInfo> cir) {
@@ -29,19 +31,7 @@ public abstract class AbstractClientPlayerEntityMixin {
             return;
         }
 
-        PlayerInfo returnValue = cir.getReturnValue();
-        if (returnValue != null) {
-            syncPlayerProfile(returnValue.getProfile());
-            cir.setReturnValue(returnValue);
-        }
-    }
-
-    private void syncPlayerProfile(GameProfile incomingProfile) {
-        GameProfile currentProfile = ((AbstractClientPlayer) (Object) this).getGameProfile();
-        if (incomingProfile == null || currentProfile == null) {
-            return;
-        }
-
-        ProfileApplier.rememberOriginalProfile(incomingProfile);
+        this.playerInfo = currentEntry;
+        cir.setReturnValue(currentEntry);
     }
 }
