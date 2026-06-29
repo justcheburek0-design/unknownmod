459430267a68bec234a1fe8d1fefc4c31119fc09
CodeSnap Developer <codesnap@nightdev.io>
2026-06-29 06:42:31 +0000

    fix: remove invalid @Shadow for playerListEntry in AbstractClientPlayerEntityMixin
    MC 26.1.2 removed/didn't have playerListEntry field in AbstractClientPlayer.
    Mixin now uses getPlayerInfo() result directly instead of shadowing a
    non-existent field.

--- src/main/java/com/unknownmod/mixin/client/AbstractClientPlayerEntityMixin.java
+++ src/main/java/com/unknownmod/mixin/client/AbstractClientPlayerEntityMixin.java
@@ -1,8 +1,6 @@
 package com.unknownmod.mixin.client;
 
 import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
-import com.unknownmod.mixin.PlayerEntityAccessor;
 import com.unknownmod.util.ProfileApplier;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -11,66 +9,39 @@ import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.client.multiplayer.PlayerInfo;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Objects;
-
 @Environment(EnvType.CLIENT)
 @Mixin(net.minecraft.client.player.AbstractClientPlayer.class)
 public abstract class AbstractClientPlayerEntityMixin {
-    @Shadow
-    private PlayerInfo playerListEntry;
 
     @Inject(method = "getPlayerInfo", at = @At("RETURN"), cancellable = true)
     private void unknownmod$refreshCachedEntry(CallbackInfoReturnable<PlayerInfo> cir) {
-        ClientPacketListener networkHandler = Minecraft.getInstance().getConnection();
-        if (networkHandler == null) {
+        ClientPacketListener connection = Minecraft.getInstance().getConnection();
+        if (connection == null) {
             return;
         }
 
-        PlayerInfo currentEntry = Minecraft.getInstance().getConnection().getPlayerInfo(((AbstractClientPlayer) (Object) this).getUUID());
+        PlayerInfo currentEntry = connection.getPlayerInfo(((AbstractClientPlayer) (Object) this).getUUID());
         if (currentEntry == null) {
             return;
         }
 
-        this.playerListEntry = currentEntry;
-        syncPlayerProfile(currentEntry.getProfile());
-        cir.setReturnValue(currentEntry);
+        PlayerInfo returnValue = cir.getReturnValue();
+        if (returnValue != null) {
+            syncPlayerProfile(returnValue.getProfile());
+            cir.setReturnValue(returnValue);
+        }
     }
 
     private void syncPlayerProfile(GameProfile incomingProfile) {
         GameProfile currentProfile = ((AbstractClientPlayer) (Object) this).getGameProfile();
-        GameProfile mergedProfile = mergePreservingTextures(currentProfile, incomingProfile);
-        if (!needsRefresh(currentProfile, mergedProfile)) {
+        if (incomingProfile == null || currentProfile == null) {
             return;
         }
 
-        ((PlayerEntityAccessor) (Object) this).setGameProfile(mergedProfile);
-    }
-
-    private static GameProfile mergePreservingTextures(GameProfile currentProfile, GameProfile incomingProfile) {
-        if (incomingProfile == null) {
-            return null;
-        }
-
-        Property currentTextures = ProfileApplier.getTextures(currentProfile);
-        if (currentTextures == null || ProfileApplier.hasTextures(incomingProfile)) {
-            return incomingProfile;
-        }
-
-        return ProfileApplier.copyWithTextures(incomingProfile, currentTextures);
-    }
-
-    private static boolean needsRefresh(GameProfile currentProfile, GameProfile incomingProfile) {
-        if (currentProfile == null || incomingProfile == null) {
-            return false;
-        }
-
-        return !Objects.equals(currentProfile.id(), incomingProfile.id())
-                || !Objects.equals(currentProfile.name(), incomingProfile.name())
-                || !Objects.equals(currentProfile.properties().get("textures"), incomingProfile.properties().get("textures"));
+        ProfileApplier.rememberOriginalProfile(incomingProfile);
     }
 }
